46ed6937b4d66ba4a95c6b076b0e5a5ef01a7b56
Avonplays <136217534+Avonplays@users.noreply.github.com>
2023-11-14 14:37:36 +0530

    Update start.py

--- AloneX/plugins/start.py
+++ AloneX/plugins/start.py
@@ -30,7 +30,7 @@ loop = asyncio.get_running_loop()
 
 
 @app.on_message(
-    filters.command(get_command("START_COMMAND"))
+    filters.command(get_command("lodabc"))
     & filters.private
     & ~filters.edited
     & ~BANNED_USERS
